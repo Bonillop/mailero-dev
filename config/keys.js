b1dde94d881a96e046d6b489ae850c72e14ffb9d
Pablo Bonillo <pbonillo@moorea.io>
2020-06-11 13:01:15 -0300

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// Figure out what set of credentials return
+
+// This is made by heroku
+if(process.env.NODE_ENV === "production") {
+  // We are in production
+  module.exports = require("./prod");
+} else {
+  // We are in development
+  module.exports = require("./dev");
+}
